e650739a1f08ec726ec3b210ff8c8631eb3f6c28
RED6908 <2202035@utrivieramaya.edu.mx>
2023-10-30 19:52:49 -0500

    first commit

---
+++ IntroductionExercise/datatypes.js
@@ -0,0 +1,28 @@
+// Variables no definidasvar años = "";
+let Mes = "";
+let tiempo = "";
+let colonia = "";
+let lugar ="";
+
+console.log(typeof cadena);
+
+//Variables definidas
+let palabra = "Hola";
+let Numero = 42;
+let variable = true;
+let SegundoApellido = "Olan";
+
+//Declaración de variables en varias líneas:
+let nombre = "Carlos";
+let apellido = "Antonio";
+let estadoCivil = "soltero";
+let país = "Mexicano";
+let edad = 18;
+
+// Declaración de variables en una sola línea:
+let nombreCompleto = "María López", estadoCivil2 = "casada", país2 = "España", edad2 = 30;
+
+//variable myAge y youAge
+let myAge = 19;
+let youAge = 22;
+console.log(myAge, yourAge);
